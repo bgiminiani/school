d33c8f6d8929e9d5338b12327e45927d72104ce9
Bruno <giminiani@gmail.com>
2023-09-09 00:17:34 -0300

    refactor validate invalid name

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,12 +1,9 @@
 import Cpf from "./Cpf";
+import Name from "./Name";
 
 export default class EnrollStudent {
   execute({ name, cpf }: { name: string; cpf: string }): void {
-    if (!/^([A-Za-z]+ )+([A-Za-z])+$/.test(name))
-      throw new Error("Invalid name");
-    const student = {
-      name,
-      cpf: new Cpf(cpf),
-    };
+    new Name(name);
+    new Cpf(cpf);
   }
 }
