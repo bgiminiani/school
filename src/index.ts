5ea8c7b08d8715aeb7b6b27277ba3b5db54ccbdd
Bruno <giminiani@gmail.com>
2023-09-09 10:56:26 -0300

    fix execute parameter on calling method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import EnrollStudent from "./EnrollStudent";
 
 const enrollStudent = new EnrollStudent();
-console.log(`User: ${enrollStudent.execute("ana")}`);
+console.log(
+  `User: ${enrollStudent.execute({
+    name: "ana",
+    cpf: "111-111-111-46",
+  })}`
+);
