a48cc78515940453613d8a6d89f879bc4cc81e40
Bruno <giminiani@gmail.com>
2023-09-09 00:13:07 -0300

    Should not enroll without valid student cpf

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -3,15 +3,31 @@ import EnrollStudent from "./EnrollStudent";
 describe("Enroll Student", () => {
   it("Should not enroll without valid student name", () => {
     const enrollStudent = new EnrollStudent();
-    expect(() => enrollStudent.execute("Ana")).toThrow(
-      new Error("Invalid student name")
-    );
+    expect(() =>
+      enrollStudent.execute({
+        name: "Ana",
+        cpf: "521.069.380-55",
+      })
+    ).toThrow(new Error("Invalid name"));
+  });
+
+  it("Should not enroll without valid student cpf", () => {
+    const enrollStudent = new EnrollStudent();
+    expect(() =>
+      enrollStudent.execute({
+        name: "Ana Maria",
+        cpf: "111.111.111-11",
+      })
+    ).toThrow(new Error("Invalid cpf"));
   });
 
   it("Should enroll student", () => {
     const enrollStudent = new EnrollStudent();
-    expect(() => enrollStudent.execute("Ana Maria")).not.toThrow(
-      new Error("Invalid student name")
-    );
+    expect(() =>
+      enrollStudent.execute({
+        name: "Ana Maria",
+        cpf: "521.069.380-55",
+      })
+    ).not.toThrow(new Error("Invalid name"));
   });
 });
--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,6 +1,12 @@
+import Cpf from "./Cpf";
+
 export default class EnrollStudent {
-  execute(name: string): void {
+  execute({ name, cpf }: { name: string; cpf: string }): void {
     if (!/^([A-Za-z]+ )+([A-Za-z])+$/.test(name))
-      throw new Error("Invalid student name");
+      throw new Error("Invalid name");
+    const student = {
+      name,
+      cpf: new Cpf(cpf),
+    };
   }
 }
