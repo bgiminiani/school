e9a755da01b1672691051155c530fa5bc3fa236c
Bruno <giminiani@gmail.com>
2023-09-19 07:49:29 -0300

    add EnrollmentRepositoryInMemory.ts

--- src/EnrollmentRepositoryInMemory.ts
+++ src/EnrollmentRepositoryInMemory.ts
@@ -1,31 +1,36 @@
+import Enrollment from "./Enrollment";
 import EnrollmentRepositoryInterface from "./EnrollmentRepositoryInterface";
 
-export default class EnrollmentRepositoryInMemory
-  implements EnrollmentRepositoryInterface
-{
-  enrollments: any[];
+export default class EnrollmentRepositoryInMemory implements EnrollmentRepositoryInterface {
+  enrollments: Enrollment[];
 
   constructor() {
     this.enrollments = [];
   }
 
-  save(enrollment: any): void {
+  save(enrollment: Enrollment): void {
     this.enrollments.push(enrollment);
   }
 
-  findByCpf(cpf: string): any {
-    return this.enrollments.find(
+  findByCpf(cpf: string): Enrollment | undefined {
+    const enrollment = this.enrollments.find(
       (enrollment) => enrollment.student.cpf.value === cpf
     );
+    return enrollment;
   }
 
-  findAllByClass(levelCode: string, moduleCode: string, classCode: string) {
-    return this.enrollments.filter(
+  findAllByClass(
+    levelCode: string,
+    moduleCode: string,
+    gradeCode: string
+  ): Enrollment[] {
+    const enrollments = this.enrollments.filter(
       (enrollment) =>
-        enrollment.level === levelCode &&
-        enrollment.module === moduleCode &&
-        enrollment.grade === classCode
+        enrollment.levelCode === levelCode &&
+        enrollment.moduleCode === moduleCode &&
+        enrollment.gradeCode === gradeCode
     );
+    return enrollments;
   }
 
   count(): number {
